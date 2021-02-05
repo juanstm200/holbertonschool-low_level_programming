e058cfcfd351cd70aea4df79aa22745b2d01cf88
juanstm200 <juans.tovar70@gmail.com>
2021-02-05 07:15:07 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (char i = 'a'; i <= 'z'; i++)
+		putchar (i);
+	return (0);
+}
