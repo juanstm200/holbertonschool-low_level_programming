ffa940c19d2d33e2348e92a5461efb986832fcc6
juanstm200 <juans.tovar70@gmail.com>
2021-02-09 02:59:14 +0000

    C_char

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,13 @@
-#include <stdio.h>
 #include "holberton.h"
 
 /**
  * print_alphabet - print alphabet of a to z
  *
  */
-
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 void print_alphabet(void)
 {
 	int i;
+
 	for (i = 97; i <= 122; i++)
 		_putchar(i);
 
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - print new lines
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i,a;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (a = 97; a <= 122; a++)
+		{
+			_putchar(a);
+		}
+
+		_putchar('\n');
+	}
+}
