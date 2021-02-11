ec8b94092e9e3cb4fb04769f1804dce432959ca1
juanstm200 <juans.tovar70@gmail.com>
2021-02-12 00:49:13 +0100

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - print diagonal line in the main
+ * @n: enter from main a integer
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar ('\\');
+		_putchar ('\n');
+	}
+
+	if (n <= 0)
+		_putchar('\n');
+}
