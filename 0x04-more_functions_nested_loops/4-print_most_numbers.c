7f4394edcb64a1a4d7fb73048229b50650ed04a3
juanstm200 <juans.tovar70@gmail.com>
2021-02-11 21:26:34 +0100

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - print number if are diferent from 2 and 4
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+
+	_putchar('\n');
+}
