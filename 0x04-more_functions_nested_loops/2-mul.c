e05db3ce21814104eaa2f05c66e739b830ec3ffa
juanstm200 <juans.tovar70@gmail.com>
2021-02-11 21:11:38 +0100

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - funcion evalue the multiplies two integers
+ * @a: fact integer, enter of main
+ * @b: fact integer, enter of main
+ * Return: result of multiplies a and b
+ *
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
