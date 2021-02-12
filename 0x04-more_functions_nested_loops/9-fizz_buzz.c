bb5f8ea993eb86765dc2e8b036b87bae33b2ff1c
juanstm200 <juans.tovar70@gmail.com>
2021-02-12 03:53:16 +0100

    9-fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
+* main - check the code for Holberton
+*
+* Return: Always 0
+*/
 
 int main(void)
 {
