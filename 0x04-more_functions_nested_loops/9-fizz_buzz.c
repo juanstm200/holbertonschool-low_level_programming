0688263c6f4055b2ecd5f4207f0ca933277e9482
juanstm200 <juans.tovar70@gmail.com>
2021-02-12 02:05:45 +0100

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		int fizz = i % 3;
+		int buzz = i % 5;
+
+		if (fizz == 0 && buzz == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (fizz == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (buzz == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((i != (fizz == 0)) && (i != (buzz == 0)))
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
