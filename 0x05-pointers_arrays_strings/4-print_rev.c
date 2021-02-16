662db8a2e0eb30922df0038c97b77dad1e491a23
juanstm200 <juans.tovar70@gmail.com>
2021-02-16 03:12:02 +0100

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: variable enter from main
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s += 1;
+	}
+
+	for (i = len; i >= 0; i--)
+	{
+		s -= 1;
+		_putchar(*s);
+	}
+	_putchar('\n');
+}
