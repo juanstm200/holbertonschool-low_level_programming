b2f8411b9dce3e84bde5d7a9d3a7ca7f3b456e2f
juanstm200 <juans.tovar70@gmail.com>
2021-02-23 04:46:05 +0100

    5-str.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,15 +10,23 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
+	char *count;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i] == needle[j])
+			if (haystack[i + j] != needle[j])
 			{
-				return (needle);
+				break;
 			}
+		}
+
+		if (!needle[j])
+		{
+			count = haystack + i;
+			return (&count[i - 1]);
+
 		}
 	}
 	return (NULL);
