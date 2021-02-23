0dfcfe1ffb0a0d1a0874e80e505a9eb1b6f950a1
juanstm200 <juans.tovar70@gmail.com>
2021-02-23 05:08:09 +0100

    comb

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,6 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+/** */
+int _putchar(char c);
+
 /*  function that fills memory with a constant byte. */
 char *_memset(char *s, char b, unsigned int n);
 
