554f01ba943a3b1fe8ba6dd344c5f048b16f0062
juanstm200 <juans.tovar70@gmail.com>
2021-02-23 03:21:33 +0100

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,16 +10,18 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
+	char *cont;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-				return (s);
+			{
+				cont = s + i;
+				return (cont);
+			}
 		}
-
-		s++;
 	}
 
 	return (NULL);
