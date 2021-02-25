9390340eb5fdc38e422b8a4d01106f04aca49c9a
juanstm200 <juans.tovar70@gmail.com>
2021-02-25 04:37:53 +0100

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion -  function that returns the value
+ * @x: Variable form enter main
+ * @y: Variable from enter main
+ * Return: pow
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+}
