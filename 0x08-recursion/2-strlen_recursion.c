c0518cd711ee27022c450ea7c30646971232dbe4
juanstm200 <juans.tovar70@gmail.com>
2021-02-25 04:00:53 +0100

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-static int cont;
 
 /**
  * _strlen_recursion - function that returns the length of a string.
@@ -10,10 +9,8 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return (cont);
+		return (0);
 	}
 
-	cont += 1;
-	cont = _strlen_recursion(s + 1);
-	return (cont);
+	return (_strlen_recursion(s + 1) + 1);
 }
