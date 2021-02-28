4a704ee3ebabe4d8abaf8c71bcb691ef60c75a52
juanstm200 <juans.tovar70@gmail.com>
2021-02-28 15:04:11 -0500

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - enter number from console by mult
+ * @argc: number of strings form argv
+ * @argv: strings enter from console number
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		int num1 = atoi(argv[1]);
+		int num2 = atoi(argv[2]);
+
+		printf("%d\n", (num1 * num2));
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
