2f91506852a4d181d30130952dd58ae2ec3ba087
juanstm200 <juans.tovar70@gmail.com>
2021-02-28 15:22:34 -0500

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - enter numbers form console
+ * @argc: number of string form argv
+ * @argv: string form console
+ * Return: 1 o 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int result = 0;
+
+	if (argc > 1)
+	{
+		for (; i <= (argc - 1); i++)
+		{
+			if (!atoi(argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+			result = result + atoi(argv[i]);
+		}
+
+		printf("%d\n", result);
+	}
+	else
+	{
+		printf("0\n");
+	}
+
+	return (0);
+}
