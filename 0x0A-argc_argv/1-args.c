43e7437abc1e7d05778bcdd16721e22c011f5429
juanstm200 <juans.tovar70@gmail.com>
2021-02-28 14:39:24 -0500

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print number of string
+ * @argc: number of string
+ * @argv: string emter form console
+ * Return: integer 0
+ */
+int main(int argc, char *argv[])
+{
+	*argv = argv[0];
+	printf("%d\n", (argc - 1));
+}
