e34b76faf1b9de307806c25892a86183cacdc53c
juanstm200 <juans.tovar70@gmail.com>
2021-03-09 05:28:40 +0100

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,14 +7,20 @@
  * @age: age of dog
  * @owner: name of person
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
 /* function that prints a struct dog */
 void print_dog(struct dog *d);
+
+/*  function that initialize a variable of type struct dog */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/* Write a function that frees dogs. */
+void free_dog(dog_t *d);
 
 #endif
