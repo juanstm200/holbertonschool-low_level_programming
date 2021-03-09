f581a45a96ad623de51da8fbcabbb8e8b2edd509
juanstm200 <juans.tovar70@gmail.com>
2021-03-09 04:35:33 +0100

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - data type of struct
+ * @name: first name
+ * @age: age of dog
+ * @owner: name of person
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
