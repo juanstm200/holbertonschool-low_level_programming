711757da72da0408e8142eb05c2dcd9d7ac5471b
juanstm200 <juans.tovar70@gmail.com>
2021-03-09 17:35:59 +0100

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,11 +12,13 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
+
 	if (d->name == NULL)
 	{
 		d->name = "(nil)";
 	}
-	else if (d->owner == NULL)
+
+	if (d->owner == NULL)
 	{
 		d->owner = "(nil)";
 	}
