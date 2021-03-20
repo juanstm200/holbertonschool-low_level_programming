ec28328af0e2d6aa09cd3967b4ea86ac3ff8dc56
juanstm200 <juans.tovar70@gmail.com>
2021-03-20 03:47:26 +0100

    Link_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - print string from link list
+ * @h: is the fist head form list
+ * Return: the number of node
+ */
+size_t print_list(const list_t *h)
+{
+	int amount = 0;
+	while(h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%i] %s\n", h->len,h->str);
+		else
+			printf("[0] (nil)\n");
+
+		h = h->next;
+		amount++;
+	}
+
+	return (amount);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,38 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+	
+} list_t;
+
+/* Write a function that prints all the elements of a list_t list.*/
+size_t print_list(const list_t *h);
+/* Write a function that returns the number of elements in linked list_t list*/
+size_t list_len(const list_t *h);
+/* Write a function that adds a new node at the beginning of a list_t list.*/
+list_t *add_node(list_t **head, const char *str);
+/* Write a function that adds a new node at the end of a list_t list.*/
+list_t *add_node_end(list_t **head, const char *str);
+/* Write a function that frees a list_t list. */
+void free_list(list_t *head);
+
+
+#endif
