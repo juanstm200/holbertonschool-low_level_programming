4b2037df08e102fbfe8d1fa1f97467ecc34bceb1
juanstm200 <juans.tovar70@gmail.com>
2021-03-20 05:58:07 +0100

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * fish - execute fish before main
+ */
+void fish(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon");
+	printf(" my back!\n");
+}
