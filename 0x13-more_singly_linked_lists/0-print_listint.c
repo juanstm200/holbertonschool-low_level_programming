c98306e5edfdb1cafd62dd925cbc621a4ea74280
juanstm200 <juans.tovar70@gmail.com>
2021-03-24 02:34:45 +0100

    more_list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - print character enter form main to program
+ * @h: is yhe date enter form main
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,57 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+	
+} listint_t;
+
+/* Write a function that prints all the elements of a listint_t list.*/
+size_t print_listint(const listint_t *h);
+
+/* Write a function that returns the number of elements in a linked listint_t*/
+size_t listint_len(const listint_t *h);
+
+/* Write a function that adds a new node at the beginning of a listint_t list.*/
+listint_t *add_nodeint(listint_t **head, const int n);
+
+/* Write a function that adds a new node at the end of a listint_t list. */
+listint_t *add_nodeint_end(listint_t **head, const int n);
+
+/* Write a function that frees a listint_t list. */
+void free_listint(listint_t *head);
+
+/* Write a function that frees a listint_t list.*/
+void free_listint2(listint_t **head);
+
+/* Write a function that deletes the head node of a listint_t*/
+int pop_listint(listint_t **head);
+
+/* Write a function that returns the nth node of a listint_t linked list.*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+
+/*Write a function that returns the sum of all the data (n) of a listint_t */
+int sum_listint(listint_t *head);
+
+/* Write a function that inserts a new node at a given position. */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+
+/*sa a function that deletes the node at index index of a listint  linked list*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+
+#endif
