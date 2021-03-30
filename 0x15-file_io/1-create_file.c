e271d13b975e63de550467b1f6e23387cba014f7
juanstm200 <juans.tovar70@gmail.com>
2021-03-30 05:52:17 +0200

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,9 +18,6 @@ int create_file(const char *filename, char *text_content)
 	if (cFile == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
-
 	if (text_content != NULL)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
