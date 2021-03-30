2a4d994c7cc110b034a74e86df46b66fd301fa2b
juanstm200 <juans.tovar70@gmail.com>
2021-03-30 05:11:44 +0200

    file_cre

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,26 +8,31 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int createFile;
+	int cFile;
 	int i, wnum = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	createFile = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	cFile = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0600);
+
+	if (cFile == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
 
 	if (text_content != NULL)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
 		{}
 
-		wnum = write(createFile, text_content, i);
-		close(createFile);
+		wnum = write(cFile, text_content, i);
+		close(cFile);
 
 		if (wnum == -1)
 			return (-1);
 	}
 
-	close(createFile);
 	return (1);
 }
