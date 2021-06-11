c6193db8936c1599e88fbf7577c9f74570d9a0a0
juanstm200 <juans.tovar70@gmail.com>
2021-06-10 23:35:10 -0500

    comb_4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints non repeating numbers from 000 to 999
+ *
+ * Return: 0 Always, on completion
+ */
+int main(void)
+{
+	int num0 = 48;
+	int num1;
+	int num2;
+	while (num0 <= 57)
+	{
+		for (num1 = 48; num1 <= 57; num1++)
+		{
+			for (num2 = 48; num2 <= 57; num2++)
+			{
+				if (num0 < num1 && num1 < num2)
+				{
+					putchar(num0);
+					putchar(num1);
+					putchar(num2);
+					if (num0 != 55 || num1 != 56 || num2
+					    != 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+		num0++;
+	}
+	putchar(10);
+	return (0);
+}
